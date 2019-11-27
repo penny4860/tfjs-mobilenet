83af0c99239661fd588f8c1227d315535c68ce0d
jeongjoonsup <penny4860@gmail.com>
2019-11-27 21:39:19 +0900

    이미지 업로드예제코드 구현

--- tfjs-mobilenet/img_upload.js
+++ tfjs-mobilenet/img_upload.js
@@ -3,30 +3,30 @@ const IMAGE_SIZE = 224;
 const CONTENTS_ELEMENT = document.getElementById('predictions');
 const FILES_ELEMENT = document.getElementById('files');
 
-document.getElementById('file-container').style.display = '';
 
-
-FILES_ELEMENT.addEventListener('change', event => {
+function fileUploadListener(event)
+{
     let files = event.target.files;
-    // Display thumbnails & issue call to predict each image.
     for (let i = 0, f; f = files[i]; i++) {
-      // Only process image files (skip non image files)
       if (!f.type.match('image.*')) {
         continue;
       }
       let reader = new FileReader();
-      reader.onload = e => {
-        let img = document.createElement('img');
-        img.src = e.target.result;
-        img.width = IMAGE_SIZE;
-        img.height = IMAGE_SIZE;
-        img.onload = () => showImage(img);
-      };
+      reader.onload = e => imgLoader(e);
   
       // Read in the image file as a data URL.
       reader.readAsDataURL(f);
     }
-  });
+}
+
+function imgLoader(event)
+{
+    let img = document.createElement('img');
+    img.src = event.target.result;
+    img.width = IMAGE_SIZE;
+    img.height = IMAGE_SIZE;
+    img.onload = () => showImage(img);
+}
 
 async function showImage(imgElement) {
   const imgContainer = document.createElement('div');
@@ -34,3 +34,7 @@ async function showImage(imgElement) {
   CONTENTS_ELEMENT.insertBefore(
       imgContainer, CONTENTS_ELEMENT.firstChild);
 }
+
+
+document.getElementById('file-container').style.display = '';
+FILES_ELEMENT.addEventListener('change', e => fileUploadListener(e));
